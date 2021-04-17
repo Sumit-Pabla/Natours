46b4de5409d022938c296725f666f82be74918c0
Sumit-Pabla <sumit.pabla@sjsu.edu>
2021-04-17 15:07:26 -0700

    Refactored for MVC format

--- server.js
+++ server.js
@@ -14,34 +14,7 @@ mongoose.connect(DB, {
   console.log(`Successful DB connection`)
 
 })
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 1.00
-})
-
-testTour.save().then((doc) => {
-  console.log(doc)
-}).catch((err) => {
-  console.log('Error!!', err)
-});
 
 //STARTING SERVER
 const port = 3000;
