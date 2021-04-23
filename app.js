52f2fe1db16bf62a20552c77c0ad42114a6e86ef
Sumit-Pabla <sumit.pabla@sjsu.edu>
2021-04-22 23:12:48 -0700

    Added data sanitization

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const globalErrorHandler = require('./controllers/errorController')
 const AppError = require('./utils/AppError')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
 
 const app = express();
 
 
 
 //Middleware
+app.use(helmet())
+
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'))
 }
@@ -21,10 +26,15 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 100,
   message: 'Too many requesest from this IP. Try again in an hour'
 });
-app.use('/api', limiter)
+app.use('/api', limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`))
+
 
 
 
