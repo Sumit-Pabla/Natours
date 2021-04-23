bd4587e29d949eab8339e5f19cfe65c4b5b95fd2
Sumit-Pabla <sumit.pabla@sjsu.edu>
2021-04-22 23:18:54 -0700

    Implemented methods to prevent parameter pollution

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const userRouter = require('./routes/userRoutes');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const app = express();
 
@@ -32,6 +33,11 @@ app.use(express.json({ limit: '10kb' }));
 
 app.use(mongoSanitize());
 app.use(xss());
+app.use(hpp({ 
+  whitelist: [
+    'duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price'
+  ]
+ }))
 
 app.use(express.static(`${__dirname}/public`))
 
