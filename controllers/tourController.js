0efb68f44faa6728508d878f3b7a01d27778d417
Sumit-Pabla <sumit.pabla@sjsu.edu>
2021-04-25 00:02:37 -0700

    Created geospatial queries and geospatial aggregation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -100,6 +100,64 @@ exports.getTourStats = catchAsync(async (req, res) => {
     });
     
 })
+
+exports.getToursWithin = catchAsync( async (req, res, next) => {
+  
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if(!lat || !lng) {
+    next( new AppError('Please provide lat and lon properly', 400 ))
+  }
+
+  const tours = await Tour.find({ startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radiu]}} })
+  
+  
+  res.status(200).json({
+    staus: 'success',
+    data: {
+      data: tours
+    }
+  })
+})
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if(!lat || !lng) {
+    next( new AppError('Please provide lat and lon properly', 400 ))
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng*1, lat*1]
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier
+      }
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1
+      }
+    }
+  ]);  
+  
+  res.status(200).json({
+    staus: 'success',
+    data: {
+      data: distances
+    }
+  })
+})
   
   // exports.createTour = catchAsync(async (req, res, next) => {
   //   // const newTours = new Tour({})
